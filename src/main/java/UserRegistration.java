a0753c90d0892fbb0da900c8a7956dfcca76eec5
Devki1 <gdev3195@gmail.com>
2020-03-05 15:43:38 +0530

    ADDED:Feature of valid first name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,11 +1,12 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class UserRegistration
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome in regex simulation");
-
+public class UserRegistration {
+    //Validate first name
+    public boolean validFirstName(String firstName) {
+        String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternValidName);
+        Matcher m = p.matcher(firstName);
+        return m.matches();
     }
 }
 
