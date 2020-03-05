573415724eab35eb94cb3055d572c1162650edd3
Devki1 <gdev3195@gmail.com>
2020-03-05 16:17:03 +0530

    ADDED:Feature a last name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,11 +1,21 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class UserRegistration {
+public class UserRegistration
+{
     //Validate first name
-    public boolean validFirstName(String firstName) {
+    public boolean validFirstName(String firstName)
+    {
         String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternValidName);
         Matcher m = p.matcher(firstName);
+        return m.matches();
+    }
+    //Validate regex for last name
+    public boolean validLastName(String lastName)
+    {
+        String patternValidLastName = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternValidLastName);
+        Matcher m = p.matcher(lastName);
         return m.matches();
     }
 }
