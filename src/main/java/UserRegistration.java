578acbba94a5a40e2886a0d33c651bea88efac9b
Devki1 <gdev3195@gmail.com>
2020-03-05 18:50:29 +0530

    MODIFIED:Added a length of password minimum 8 character

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,37 +2,40 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistration {
-        //Validate first name
-        String COUNTRY_CODE = "91";
-        public boolean validFirstName (String firstName)
-        {
-            String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
-            Pattern p = Pattern.compile(patternValidName);
-            Matcher m = p.matcher(firstName);
-            return m.matches();
-        }
-        //Validate regex for last name
-        public boolean validLastName (String lastName)
-        {
-            String patternValidLastName = "^[A-Z]{1}[a-z]{2,}$";
-            Pattern p = Pattern.compile(patternValidLastName);
-            Matcher m = p.matcher(lastName);
-            return m.matches();
-        }
-        //Validate pattern for emailid
-        public boolean validEmailId (String emialId)
-        {
-            String patternValidEmailId = "^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
-            Pattern p = Pattern.compile(patternValidEmailId);
-            Matcher m = p.matcher(emialId);
-            return m.matches();
-        }
-        public boolean validMobileNumber(String mobileNumber)
-        {
-            String patternValidMobileNumber = "^"+COUNTRY_CODE+ "[ ][9876]{1}[0-9]{9}";
-            Pattern p = Pattern.compile(patternValidMobileNumber);
-            Matcher m = p.matcher(mobileNumber);
-            return m.matches();
-        }
+    //Validate first name
+    String COUNTRY_CODE = "91";
+    String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
+    String patternValidLastName = "^[A-Z]{1}[a-z]{2,}$";
+    String patternForEmailId = "^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
+    String patternValidMobileNumber = "^" + COUNTRY_CODE + "[ ][9876]{1}[0-9]{9}";
+    String patternValidLengthPassword = "^.{8,}$";
+
+    public boolean validator(String pattern, String user) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(user);
+        return m.matches();
     }
+
+    public boolean validFirstName(String firstName) {
+        return validator(patternValidName, firstName);
+    }
+
+    //Validate regex for last name
+    public boolean validLastName(String lastName) {
+        return validator(patternValidLastName, lastName);
+    }
+
+    //Validate pattern for emailid
+    public boolean validEmailId(String emialId) {
+        return validator(patternForEmailId, emialId);
+    }
+
+    public boolean validMobileNumber(String mobileNumber) {
+        return validator(patternValidMobileNumber, mobileNumber);
+    }
+
+    public boolean validLengthPassword(String lengthPassword) {
+        return validator(patternValidLengthPassword, lengthPassword);
+    }
+}
 
