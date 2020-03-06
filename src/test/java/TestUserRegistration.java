7f3f4515f0d23eed849fcdfe1421f4b48a461d27
Devki1 <gdev3195@gmail.com>
2020-03-06 09:29:19 +0530

    MODIFIED:Added to test a valid special character

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -10,53 +10,60 @@ public class TestUserRegistration
         boolean result = validator.validFirstName("Dev");
         Assert.assertEquals(true, result);
     }
-
     @Test
+    //Test case for last name
     public void givenLastName_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validLastName("Raj");
         Assert.assertEquals(true,result);
     }
-
     @Test
+    //Test case for validating password emailid
     public void givenEmailId_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validEmailId("gdev826@gmail.com");
         Assert.assertEquals(true,result);
     }
-
     @Test
+    //Test case for validating password for mobile number
     public void givenMobileNumber_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validMobileNumber("91 9415664675");
         Assert.assertEquals(true,result);
     }
-
     @Test
+    //Test case for validating password for proper length
     public void givenLengthPassword_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validLengthPassword("rahulrajrahul");
         Assert.assertEquals(true,result);
     }
-
     @Test
+    //Test case for validating password atleast one upper case
     public void givenUpperCase_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.upperCaseAtleastOne("rahulRajrahul");
         Assert.assertEquals(true,result);
     }
-
     @Test
+    //Test case for validating password for atleast one number
     public void givenNumericValue_WhenProper_ShouldReturnTrue()
     {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.atleastOneNumericValue("rahulRaj5rahul");
         Assert.assertEquals(true,result);
-
+    }
+    @Test
+    //Test case for validating password for atleast one special character
+    public void givenSpecialCharacter_WhenProper_ShouldReturnTrue()
+    {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.atleastOneSpecialCharacter("rahulRaj5@rahul");
+        Assert.assertEquals(true,result);
     }
 }
